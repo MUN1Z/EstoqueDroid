da6f01b3db632ef45fa4b14b6f36b0182ec5618a
sirmuniz <mrmunz2012@gmail.com>
2016-03-07 02:02:49 -0300

    Declarado variaveis na main activity..

--- app/src/main/java/br/com/felipemuniz/estoquedroid/view/MainActivity.java
+++ app/src/main/java/br/com/felipemuniz/estoquedroid/view/MainActivity.java
@@ -2,11 +2,19 @@ package br.com.felipemuniz.estoquedroid.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import br.com.felipemuniz.estoquedroid.R;
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.btnName)
+    Button mBtnName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btnName)
+    public void mBtnName(View view){
+        Toast.makeText(this, "Teste", Toast.LENGTH_SHORT).show();
+    }
+
 }
