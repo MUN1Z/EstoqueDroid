8748e720851aefb45f445b039f6570fdd073af5d
sirmuniz <mrmunz2012@gmail.com>
2016-03-07 01:38:47 -0300

    Criado a classe responsavel pelas configurações, e a responsavel pelas requisicoes http

---
+++ app/src/main/java/br/com/felipemuniz/estoquedroid/helper/Config.java
@@ -0,0 +1,33 @@
+package br.com.felipemuniz.estoquedroid.helper;
+
+/**
+ * Config [HELPER]
+ * Class responsible for HTTP requests
+ * copyright (c) 2016, Felipe Muniz, EstoqueDroid
+ */
+
+public class Config {
+
+    //Address of our scripts of the CRUD
+    public static final String URL_REGISTER="http://localhost/EstoqueDroid-WebService/_view/RegisterProduct.php";
+    public static final String URL_VIEW_ALL = "";
+    public static final String URL_VIEW_PRODUCT = "";
+    public static final String URL_UPDATE = "";
+    public static final String URL_DELETE = "";
+
+    //Keys that will be used to send the request to php scripts
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_PRICE = "price";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_DESCRIPTION = "description";
+    public static final String TAG_PRICE = "price";
+
+    //id to pass with intent
+    public static final String EMP_ID = "emp_id";
+}
--- app/src/main/java/br/com/felipemuniz/estoquedroid/view/MainActivity.java
+++ app/src/main/java/br/com/felipemuniz/estoquedroid/view/MainActivity.java
@@ -1,8 +1,9 @@
-package br.com.felipemuniz.estoquedroid;
+package br.com.felipemuniz.estoquedroid.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.felipemuniz.estoquedroid.R;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
