ad146452ac3abc2cbadd2a2cb0fd3729562096eb
sirmuniz <mrmunz2012@gmail.com>
2016-03-18 02:19:57 -0300

    Criado a activity para ver os produtos.

---
+++ app/src/main/java/br/com/felipemuniz/estoquedroid/view/ProductsActivity.java
@@ -0,0 +1,15 @@
+package br.com.felipemuniz.estoquedroid.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.felipemuniz.estoquedroid.R;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
