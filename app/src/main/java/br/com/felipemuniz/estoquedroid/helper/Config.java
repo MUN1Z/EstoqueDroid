e48d60d6589848515fbca0e7d282f5607584fbc5
sirmuniz <mrmunz2012@gmail.com>
2016-03-18 02:32:17 -0300

    Criado o método que lista os produtos obtendo o objeto json.

--- app/src/main/java/br/com/felipemuniz/estoquedroid/helper/Config.java
+++ app/src/main/java/br/com/felipemuniz/estoquedroid/helper/Config.java
@@ -10,7 +10,7 @@ public class Config {
 
     //Address of our scripts of the CRUD
     public static final String URL_REGISTER="http://estoquedroid.kingsdeveloping.com.br/_view/RegisterProduct.php";
-    public static final String URL_VIEW_ALL = "";
+    public static final String URL_GET_ALL = "http://estoquedroid.kingsdeveloping.com.br/_view/getProducts.php";
     public static final String URL_VIEW_PRODUCT = "";
     public static final String URL_UPDATE = "";
     public static final String URL_DELETE = "";
@@ -22,7 +22,7 @@ public class Config {
     public static final String KEY_PRICE = "price";
 
     //JSON Tags
-    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_JSON_ARRAY="produto";
     public static final String TAG_ID = "id";
     public static final String TAG_NAME = "name";
     public static final String TAG_DESCRIPTION = "description";
--- app/src/main/java/br/com/felipemuniz/estoquedroid/view/ProductsActivity.java
+++ app/src/main/java/br/com/felipemuniz/estoquedroid/view/ProductsActivity.java
@@ -1,15 +1,101 @@
 package br.com.felipemuniz.estoquedroid.view;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import br.com.felipemuniz.estoquedroid.R;
+import br.com.felipemuniz.estoquedroid.helper.Config;
+import br.com.felipemuniz.estoquedroid.helper.RequestHandler;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ProductsActivity extends AppCompatActivity {
+
+    @Bind(R.id.lstProducts)
+    ListView mLstProducts;
+
+    private String JSON_STRING;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+        ButterKnife.bind(this);
+        getJSON();
+    }
+
+
+    private void showProducts(){
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
+
+            for(int i = 0; i<result.length(); i++){
+                JSONObject jo = result.getJSONObject(i);
+                String id = jo.getString(Config.TAG_ID);
+                String preco = jo.getString(Config.TAG_PRICE);
+                String name = jo.getString(Config.TAG_NAME);
+
+                HashMap<String,String> employees = new HashMap<>();
+                employees.put(Config.TAG_ID,id);
+                employees.put(Config.TAG_PRICE,preco);
+                employees.put(Config.TAG_NAME,name);
+                list.add(employees);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+
+                ProductsActivity.this, list, R.layout.list_item,
+                new String[]{Config.TAG_NAME, Config.TAG_PRICE},
+                new int[]{R.id.id, R.id.name});
+
+        mLstProducts.setAdapter(adapter);
+    }
+
+    private void getJSON(){
+        class GetJSON extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(ProductsActivity.this,"Carregando...","Aguarde...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                JSON_STRING = s;
+                showProducts();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequest(Config.URL_GET_ALL);
+                return s;
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute();
     }
 }
